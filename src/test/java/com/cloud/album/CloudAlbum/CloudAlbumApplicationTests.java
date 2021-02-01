9459a6e861c1932965d100ca4d1f7c798e6e2dd0
Jacky <964413011@qq.com>
2021-02-01 15:00:59 +0800

    init project

---
+++ src/test/java/com/cloud/album/CloudAlbum/CloudAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.album.CloudAlbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
